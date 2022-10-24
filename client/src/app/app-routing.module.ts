b71ae5e4e7c44b9995cbf5cd73647069a69f57e9
David Lima <David.lima17@gmail.com>
2022-10-24 02:50:41 -0500

    Added some basic client side error handling

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoudComponent } from './core/not-foud/not-foud.component';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
-import { ProductDetailsComponent } from './shop/product-details/product-details.component';
-import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [{path: '', component: HomeComponent},
+                        {path: 'test-error', component: TestErrorComponent},
+                        {path: 'not-found', component: NotFoudComponent},
+                        {path: 'server-error', component: ServerErrorComponent},
                         {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},
                         {path: '**', redirectTo: '', pathMatch: 'full'}];
 
