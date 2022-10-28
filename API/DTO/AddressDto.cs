3eda79eed77cc7294a9480c63ae6b1782466a2c1
David Lima <David.lima17@gmail.com>
2022-10-27 23:48:09 -0500

    Added Validation for API and Identity documentation for Swagger

--- API/DTO/AddressDto.cs
+++ API/DTO/AddressDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,16 @@ namespace API.DTO
 {
     public class AddressDto
     {
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        [Required]
         public string Street { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string Province { get; set; }
+        [Required]
         public string PostalCode { get; set; }
     }
 }
